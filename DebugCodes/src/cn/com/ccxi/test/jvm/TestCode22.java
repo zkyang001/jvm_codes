5890861391a872def984087ad5b3d383e4a9c1b5
zkyang <zkyang@ccxi.com.cn>
2020-06-05 12:52:43 +0800

    2020-06-05 视频23 测试修改扩展类加载器的加载路径

---
+++ DebugCodes/src/cn/com/ccxi/test/jvm/TestCode22.java
@@ -0,0 +1,40 @@
+package cn.com.ccxi.test.jvm;
+
+/**
+ * 该类用于测试扩展类加载器的加载路径
+ * 扩展类加载器无法直接加载.class文件，只能加载jar包中的class文件
+ * 
+ * @author 86135
+ *
+ */
+public class TestCode22 {
+    
+    static {
+        System.out.println("TestCode22 inited!");
+    }
+
+    /**
+     * 测试1：在 cmd中启动， 命令修改为 java -Djava.ext.dirs=./ cn.com.ccxi.test.jvm.TestCode22
+     * 加载器是AppClassLoader是因为，扩展类加载器无法直接加载class文件
+     * ------------------------------------------------------------------------------------------------------
+     * TestCode22 inited!
+     * sun.misc.Launcher$AppClassLoader@6276e1db
+     * sun.misc.Launcher$AppClassLoader@6276e1db
+     * ------------------------------------------------------------------------------------------------------
+     * 
+     * 测试2：将TestCode01打包成jar包： jar cvf testjvm.jar ./cn/com/ccxi/test/jvm/TestCode01.class
+     * 在 cmd中启动， 命令修改为 java -Djava.ext.dirs=./ cn.com.ccxi.test.jvm.TestCode22
+     * ------------------------------------------------------------------------------------------------------
+     * TestCode22 inited!
+     * sun.misc.Launcher$AppClassLoader@302b2c81
+     * sun.misc.Launcher$ExtClassLoader@6276e1db -->AppClassLoader委托给父类ExtClassLoader加载器去加载该类
+     * ------------------------------------------------------------------------------------------------------
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println(TestCode22.class.getClassLoader());
+        
+        System.out.println(TestCode01.class.getClassLoader());
+    }
+}
