d2ab689920ae33076e803bc0f9de404aa076a302
zkyang.home <yangzk2803@163.com>
2020-07-15 23:57:37 +0800

    2020-07-15 jvm-065

---
+++ DebugCodes/src/cn/com/ccxi/test/memory/TestCode03.java
@@ -0,0 +1,31 @@
+package cn.com.ccxi.test.memory;
+
+public class TestCode03 {
+	public static void main(String[] args) {
+		new Thread(() -> A.method(),  "thread-A").start();
+		new Thread(() -> B.method(),  "thread-B").start();
+	}
+}
+class A {
+	public static synchronized void method() {
+		System.out.println(" class A method .");
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		B.method();
+	}
+}
+
+class B {
+	public static synchronized void method() {
+		System.out.println(" class B method .");
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		A.method();
+	}
+}
